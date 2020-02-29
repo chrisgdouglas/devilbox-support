cdacff58cdbacf95ca96e956a904d7e1757a0d21
Chris Douglas <chrisgdouglas@gmail.com>
2020-02-29 15:02:56 -0600

    Check: docker running prior to launching devilbox

--- devilstart.sh
+++ devilstart.sh
@@ -2,8 +2,19 @@
 
 devilbox_path=~/code/devilbox
 curr_dir=$(pwd)
+docker_down=$(systemctl show --property ActiveState docker | grep -c inactive)
 
-cd $devilbox_path
-./start.sh
-cd $curr_dir
+if [ "$docker_down" == "1" ];
+    then
+        sudo /usr/bin/systemctl start docker
+        docker_down=0
+fi 
 
+if [ "$docker_down" == "0" ];
+    then
+        cd $devilbox_path
+        ./start.sh
+        cd $curr_dir
+    else 
+        echo "Docker isn't running. Check system logs."
+fi
