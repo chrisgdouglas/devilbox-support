5bcc329da3fb66dba15ba4eff409b9f9f1fd8148
Chris Douglas <chrisgdouglas@gmail.com>
2020-03-03 12:27:23 -0600

    Updates after running ShellCheck linter

--- newproj.sh
+++ newproj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set variables
-project_path=~/code/projects
-devilbox_path=~/code/devilbox
+project_path=/home/cgrd/code/projects
+devilbox_path=/home/cgrd/code/devilbox
 docker_down=$(systemctl show --property ActiveState docker | grep -c inactive)
 curr_dir=$(pwd)
 if sudo -nv 2>/dev/null && sudo -v ; then
@@ -10,19 +10,19 @@ else
     sudo_active=0
 fi
 echo "Creating project paths."
-mkdir $project_path/$1
-mkdir $project_path/$1/htdocs
-mkdir $project_path/$1/.vscode
+mkdir $project_path/"$1"
+mkdir $project_path/"$1"/htdocs
+mkdir $project_path/"$1"/.vscode
 echo "Creating new project git."
-git init $project_path/$1/htdocs
+git init $project_path/"$1"/htdocs
 echo "Creating placeholder html file."
-touch $project_path/$1/htdocs/index.html
+touch $project_path/"$1"/htdocs/index.html
 echo "Preparing project config files."
-cp $project_path/default.code-workspace $project_path/$1.code-workspace
-cp -r $project_path/.vscode $project_path/$1/
+cp $project_path/default.code-workspace $project_path/"$1".code-workspace
+cp -r $project_path/.vscode $project_path/"$1"/
 #replace placeholder in defaults with project specific paths
-sed -i "s|REPLACE|${1}|g" $project_path/$1.code-workspace
-sed -i "s|REPLACE|${1}|g" $project_path/$1/.vscode/launch.json
+sed -i "s|REPLACE|${1}|g" $project_path/"$1".code-workspace
+sed -i "s|REPLACE|${1}|g" $project_path/"$1"/.vscode/launch.json
 echo "Updating /etc/hosts with a new entry for project. Access http://$1.loc in your browser."
 if [ "$sudo_active" == "1" ] ; 
 then
@@ -38,11 +38,11 @@ then
 else
     echo "Error: Can't start docker."
 fi
-if [ "$docker_down == "0" ] ;
+if [ "$docker_down" == "0" ] ;
 then
-    cd $devilbox_path
+    cd "$devilbox_path" || exit
     ./start.sh
     #go back home
-    cd $curr_dir
+    cd "$curr_dir" || exit
 fi
 echo "Complete."
